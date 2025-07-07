458337e5330536f7c8eecbd0baca84ca24a89ac9
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-07 08:07:26 +0100

    first security commit by engr ipaye

---
+++ src/main/java/org/engripaye/iamoauth2protocol/IamOauth2protocolApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.iamoauth2protocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamOauth2protocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamOauth2protocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/engripaye/iamoauth2protocol/IamOauth2protocolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.iamoauth2protocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamOauth2protocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
